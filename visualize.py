e03380804f50118e66c1600c8e8f1328c2040843
ahmetozturk <ahmet.ozturk@atarlabs.io>
2019-03-20 21:21:28 +1100

    images: - Improved quality for sample word cloud images
    visualize.py:
    - Improved image quality and reduced border padding

--- visualize.py
+++ visualize.py
@@ -5,10 +5,12 @@ from wordcloud import WordCloud, STOPWORDS
 def createWordCloud(text):
     try:
         print('Generating word cloud\n')
-        cloud = WordCloud(max_words=500, background_color="black").generate(text)
+        cloud = WordCloud(max_words=200, width=1600, height=800).generate(text)
 
         # Display the generated image:
+        plt.figure(figsize=(20, 10), facecolor='k')
         plt.imshow(cloud, interpolation='bilinear')
+        plt.tight_layout(pad=0)
         plt.axis("off")
         plt.show()
 #        wordcloud.to_file('images/mongodb-wordcloud.png')
